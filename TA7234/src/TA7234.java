d8396074f99f03e59e1e70b3852edb6712f45251
DAMJuncosa <71723517+DAMJuncosa@users.noreply.github.com>
2022-01-17 09:32:28 +0100

    Falta 2 y 4

--- TA7234/src/TA7234.java
+++ TA7234/src/TA7234.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 import javax.swing.JOptionPane;
 
 public class TA7234 {
-	HashMap<String, ArrayList<Double>> carrito = new HashMap<>();
+	static HashMap<String, ArrayList<Double>> carrito = new HashMap<>();
+	static HashMap<String, ArrayList<Double>> almacen = new HashMap<>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,5 +16,38 @@ public class TA7234 {
 	//2
 	
 	//Añadimos productos a la lista de la compra
+	public static void añadirCompra() {
+		
+	}
+	//3
+	//Añadimos productos al almazen
+	public static void añadirAlmazen() {
+		String nombre = JOptionPane.showInputDialog("Nombre del producto");
+		double precio = Double.valueOf(JOptionPane.showInputDialog("Numero de objetos"));
+		double cantidad = Double.valueOf(JOptionPane.showInputDialog("Numero de objetos"));
+		int confirmado = JOptionPane.showConfirmDialog(null,"¿Añadir otro producto?");
+		ArrayList<Double> infoProducto = new ArrayList<>();
+		infoProducto.add(precio);
+		infoProducto.add(cantidad);
+		
+		 almacen.put(nombre, infoProducto);
+		
+		if (confirmado == JOptionPane.OK_OPTION) {
+			añadirAlmazen();
+		} else {
+			mostrar(almacen);
+		}
+		
+	}
+	//Mostramos el contenido del almacen
+	public static void mostrar(HashMap<String, ArrayList<Double>> mostrador) {
+
+		for(String id :mostrador.keySet()) {
+			System.out.println("Producto " + id + " precio " + mostrador.get(id).get(0) +" cantidad " + mostrador.get(id).get(1));
+		}
+	}
+	
+	// 4
+	
 	
 }
